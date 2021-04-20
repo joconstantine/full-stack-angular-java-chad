13d90dbb7dced866b4fc02f72784e666357c393f
joconstantine <joconstantine@users.noreply.github.com>
2021-04-20 22:41:16 +0800

    20/04/2021 22:41

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  products: Product[];
+  productListSub: Subscription;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  ngOnDestroy(): void {
+    if (this.productListSub) {
+      this.productListSub.unsubscribe();
+    }
+  }
+
+  listProducts() {
+    this.productListSub = this.productService.getProductList().subscribe(
+      (data: Product[]) => {
+        this.products = data;
+      }
+    );
+  }
+
+}
